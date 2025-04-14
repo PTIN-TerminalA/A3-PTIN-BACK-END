e04014fb0c51004129f141dd8e09c49143681e40
danifeeerr <danielferp2004@gmail.com>
2025-04-14 18:49:05 +0200

    s

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from app.database import Base
 
 class User(Base):
@@ -7,3 +8,5 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)  # auto_increment
     email = Column(String(100), unique=True, index=True)  # UNIQUE constraint
     password = Column(String(255), nullable=False)  # NO NULL constraint
+
+    regular = relationship("Regular", back_populates="user", uselist=False)
